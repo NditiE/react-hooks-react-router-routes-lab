5a7dba972d859b76e86c62deb5185b1bf5a2cbbd
NditiE <ellamarlene54@gmail.com>
2023-11-06 12:33:37 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,7 +7,32 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/actors">Actors</Link>
+          </li>
+          <li>
+            <Link to="/directors">Directors</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+        </ul>
+      </nav>
+      <Route path="/" exact component={Home} />
+      <Route path="/actors" component={Actors} />
+      <Route path="/directors" component={Directors} />
+      <Route path="/movies" component={Movies} />
+    </div>
+  );
 }
 
 export default App;
+
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      <ul>
+        {directors.map((director, directorIndex) => (
+          <li key={directorIndex}>
+            {director.name}
+            <ul>
+              {director.movies.map((movie, movieIndex) => (
+                <li key={movieIndex}>{movie}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Directors;
